5042ad7d123b889c00b0a53016250963e728613c
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2022-04-15 01:26:14 +0530

    minor updates

--- experiments/rcodesearch/etags2json.R
+++ experiments/rcodesearch/etags2json.R
@@ -76,7 +76,7 @@ table2json <-
     if (limit > 0) tag_data <- head(tag_data, limit)
     ## str(tag_data)
     HREF <- with(tag_data,
-                 sprintf("<a href='display.html?file=%s&line=%s' target='_blank'>%s</a>",
+                 sprintf("<a href='display.html?file=%s#line.%s' target='_blank'>%s</a>",
                          URLencode(srcfile, reserved = TRUE), startline, srcfile))
     ans <- list(data = vector(mode = "list", length = nrow(tag_data)))
     for (i in seq_len(nrow(tag_data)))
